5d7d4c476926d937e8bfeb8cf9540e8ecfadc335
AmZzJay <nudthaphon.zj@gmail.com>
2026-03-09 04:03:09 +0700

    Add AI C# Script ( Fly )

---
+++ Assets/Scripts/fily.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class FlyAndRotate : MonoBehaviour
+{
+    public float flySpeed = 15f;
+    public float rotateSpeed = 100f;
+
+    void Update()
+    {
+        // บินไปทางแกน -X
+        transform.Translate(Vector3.left * flySpeed * Time.deltaTime);
+
+        // บิดหมุนตามแกน X
+        transform.Rotate(-rotateSpeed * Time.deltaTime, 0f, 0f);
+    }
+}
